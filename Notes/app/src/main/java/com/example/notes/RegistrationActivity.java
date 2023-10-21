b61a6d3687adcc76733b8e661ba680c6233d3c08
Rohit <rohit055428@gmail.com>
2023-10-21 22:18:02 +0530

    new

--- Notes/app/src/main/java/com/example/notes/RegistrationActivity.java
+++ Notes/app/src/main/java/com/example/notes/RegistrationActivity.java
@@ -1,14 +1,69 @@
 package com.example.notes;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText emailEditText, passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        mAuth = FirebaseAuth.getInstance();
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+    }
+
+    // Method to handle user registration
+    public void registerUser(View view) {
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        // Check for empty email or password
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Create a new user with email and password
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Registration success, update UI and navigate to the main activity
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            if (user != null) {
+                                String userId = user.getUid();
+                                // You can store user data in the Firebase Realtime Database or Firestore here.
+                            }
+                            Toast.makeText(RegistrationActivity.this, "Registration successful!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+                        } else {
+                            // Registration failed, display an error message
+                            Toast.makeText(RegistrationActivity.this, "Registration failed. Please try again.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    // Method to navigate to the login activity
+    public void goToLogin(View view) {
+        startActivity(new Intent(this, LoginActivity.class));
     }
 }
